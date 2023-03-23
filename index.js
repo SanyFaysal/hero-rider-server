e88ccf080a26016baa1dd0c8d0c2978db50711b8
SanyFaysal <tfoysalahmed@gmail.com>
2023-03-23 11:59:05 +0600

    fix:mngoose connect

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const colors = require("colors");
 
 const app = require("./app");
 
-// mongoose.connect(process.env.DATABASE).then(() => {
-//   console.log(`Database is connected successfuly`.blue.bold);
-// });
+mongoose.connect(process.env.DATABASE).then(() => {
+  console.log(`Database is connected successfuly`.blue.bold);
+});
 
-try {
-  mongoose.connect(
-    process.env.DATABASE,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => console.log(`Mongoose is connected`.blue.bold)
-  );
-} catch (e) {
-  console.log(`could not connect`.red.bold);
-}
+// try {
+//   mongoose.connect(
+//     process.env.DATABASE,
+//     { useNewUrlParser: true, useUnifiedTopology: true },
+//     () => console.log(`Mongoose is connected`.blue.bold)
+//   );
+// } catch (e) {
+//   console.log(`could not connect`.red.bold);
+// }
 
 // server
 const port = process.env.PORT || 8080;
